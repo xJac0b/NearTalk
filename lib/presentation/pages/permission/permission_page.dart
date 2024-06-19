24caae544c98b916acf98a6ed29b176b6aab68ea
xJac0b <kubap1402@gmail.com>
2024-06-19 17:23:02 +0200

    fix: splash page animation

--- lib/presentation/pages/permission/permission_page.dart
+++ lib/presentation/pages/permission/permission_page.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooked_bloc/hooked_bloc.dart';
 import 'package:neartalk/presentation/pages/permission/cubit/permission_cubit.dart';
 import 'package:neartalk/presentation/pages/permission/widgets/permission_view.dart';
 import 'package:neartalk/presentation/pages/permission/widgets/splash_view.dart';
-import 'package:neartalk/presentation/router/routes.dart';
 
 class PermissionPage extends HookWidget {
   const PermissionPage({super.key});
@@ -14,13 +12,6 @@ class PermissionPage extends HookWidget {
   Widget build(BuildContext context) {
     final cubit = useBloc<PermissionCubit>();
     final state = useBlocBuilder(cubit);
-
-    useActionListener(
-        cubit,
-        (action) => switch (action) {
-              GoHome() => context.go(Routes.home),
-              _ => null,
-            });
 
     // useEffect(
     //   () {
--- lib/presentation/pages/permission/widgets/splash_view.dart
+++ lib/presentation/pages/permission/widgets/splash_view.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooked_bloc/hooked_bloc.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:neartalk/presentation/pages/permission/cubit/permission_cubit.dart';
+import 'package:neartalk/presentation/router/routes.dart';
 import 'package:neartalk/presentation/shared/extensions/app_theme_extension.dart';
 import 'package:neartalk/presentation/styles/app_spacings.dart';
 import 'package:neartalk/presentation/styles/app_typography.dart';
@@ -27,6 +30,18 @@ class SplashView extends HookWidget {
       });
       return null;
     }, []);
+
+    useActionListener(
+        cubit,
+        (action) async => switch (action) {
+              GoHome() => {
+                  visible.value = false,
+                  await Future.delayed(const Duration(milliseconds: 400), () {
+                    context.go(Routes.home);
+                  }),
+                },
+              _ => null,
+            });
 
     return Scaffold(
       body: AnimatedOpacity(
